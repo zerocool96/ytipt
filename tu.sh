5f0eff038479bde357876d5e158f9120be617a58
zerocool96 <52238093+zerocool96@users.noreply.github.com>
2023-12-14 23:25:19 +0530

    Update tu.sh

--- tu.sh
+++ tu.sh
@@ -3,14 +3,13 @@
 echo $(dirname $0)
 
 # Install dependencies
-python3 -m pip install asyncio
-python3 -m pip install argparse
+
 python3 -m pip install playwright
 python3 -m pip install requests
 
 # Navigate to the scripts folder
 cd $(dirname $0)/scripts/
 
-python3 tu.py
+python3 tu.py > ../m3u8_urls.txt
 
 echo m3u8 grabbed
